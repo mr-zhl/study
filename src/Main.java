86f2a3b5d8c87e1cfb44011c7c87da1e1b51017d
zhaohongliang <1971169846@qq.com>
2019-02-18 10:05:26 +0800

    注释

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        //版本控制系统:VCS,Version control System
+        //1.集中式版本控制系统:svn,subversion(向服务器提交,不能向自身提交)
+        //2.分布式版本控制系统:Git(向服务器提交,也能向自身提交)
+
+
+        //svn服务器:taoCode,SVNchina
+        //Git服务器:gitee,github
+
+        //Git使用
+
+        //Git安装:
+        //下载地址:https://git-scm.com/download/win
+
+        //初始版本化
+        //git init
+        //注:当前文件夹,有文件要进行版本控制
+        //(git:(master))
+
+
+        //查看当前版本库的状态
+        //git status
+
+        //指定某个文件进行版本控制
+        //git add 文件
+
+    }
+}
