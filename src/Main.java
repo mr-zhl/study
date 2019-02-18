49e74037bfb91bb1a35048e6f2d79917989b1bd0
zhaohongliang <1971169846@qq.com>
2019-02-18 10:46:36 +0800

    第一次提交

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,42 @@ public class Main {
         //指定某个文件进行版本控制
         //git add 文件
 
+        //红色:未进行版本控制
+        //绿色:新增文件
+        //蓝色:修改文件
+
+        //git配置
+        //git config --global user.name "姓名"
+        //git config --global user.email "邮箱"
+        //git config --global user.email "1971169846@qq.com"
+
+
+        //删除git信息配置
+        //git config --global --unset user.email
+
+
+        //查看git信息配置
+        //git config --global --list
+
+        //注:
+        //--system:系统
+        //--global:当前用户
+        //--local:当前仓库
+
+        //提交修改
+        //git commit -m "注释"
+
+        //和上一个版本进行比较,查看修改文件信息
+        //git diff 文件(比如:git diff src/Main.java)
+        //注:按q退出查看
+
+        //export LANG="zh_CN.UTF-8"
+        //export LC_ALL="zh_CN.UTF-8"
+
+
+
+
+
+
     }
 }
